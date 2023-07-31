c743eb79d93f526573a6b4f666da72736c1e9541
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2023-07-31 12:59:54 +0900

    Add files via upload

---
+++ leds_buttons.ino
@@ -0,0 +1,53 @@
+const int numLEDs = 8; // Number of LEDs connected
+int ledPins[] = {4, 5, 6, 7, 8, 9, 10, 11}; // Array to store the LED pin numbers
+
+const int stopButtonPin = 12; // Pin number for the stop button
+const int startButtonPin = 13; // Pin number for the start button
+
+boolean blinkEnabled = true; // Flag to control blinking
+
+void setup() {
+  // Initialize all LED pins as OUTPUT
+  for (int i = 0; i < numLEDs; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
+
+  // Initialize push buttons as INPUT with internal pull-up resistors
+  pinMode(stopButtonPin, INPUT_PULLUP);
+  pinMode(startButtonPin, INPUT_PULLUP);
+}
+
+void loop() {
+  // Checking if the stop button is pressed
+  if (digitalRead(stopButtonPin) == LOW) {
+    blinkEnabled = false; // Disable blinking
+    turnOffLEDs(); // Turn off all LEDs
+    while (digitalRead(stopButtonPin) == LOW) {
+      // Wait until the stop button is released
+    }
+  }
+
+  // Check if the start button is pressed
+  if (digitalRead(startButtonPin) == LOW) {
+    blinkEnabled = true; // Enable blinking
+    while (digitalRead(startButtonPin) == LOW) {
+      // Wait until the start button is released
+    }
+  }
+
+  // Blink LEDs if blinking is enabled
+  if (blinkEnabled) {
+    for (int i = 0; i < numLEDs; i++) {
+      digitalWrite(ledPins[i], HIGH); // Turn on the current LED
+      delay(500);// Wait for half a second
+      digitalWrite(ledPins[i], LOW); // Turn off the current LED
+    }
+  }
+}
+
+// Function to turn off all LEDs
+void turnOffLEDs() {
+  for (int i = 0; i < numLEDs; i++) {
+    digitalWrite(ledPins[i], LOW);
+  }
+}
